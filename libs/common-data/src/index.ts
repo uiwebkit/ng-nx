9004250642d0b4f7b05f38b40603af8958785369
vital <uniwebkit@gmail.com>
2024-12-12 12:26:05 +0200

    rename errorHandler file name

--- libs/common-data/src/index.ts
+++ libs/common-data/src/index.ts
@@ -1,6 +1,5 @@
 export * from './lib/utils/is';
-export * from './lib/utils/rx-custom-pipes';
-export * from './lib/utils/rx-unsubscribe';
+export * from './lib/utils/rx-error-handler';
 
 export * from './lib/features/loading/loading.interceptor';
 export * from './lib/features/loading/loading.service';
